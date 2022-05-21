9f2f88ffe8789cb34f7a151bf65ff6545f39dc70
Pablo Perales Molas <pperalesm@gmail.com>
2022-05-21 19:18:18 +0200

    Added local env variables

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM node:16
 WORKDIR /app
 COPY package.json .
-RUN npm install
+RUN npm install && npm run build
 COPY . .
 EXPOSE 3000
 CMD npm run start:prod
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { MongooseModule } from "@nestjs/mongoose";
 import { AccountsModule } from "./accounts/accounts.module";
 import { CodesModule } from "./codes/codes.module";
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ ignoreEnvFile: process.env.ENV != "dev" }),
     MongooseModule.forRoot(
       `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_URL}/accounts-microservice?authSource=admin`,
     ),
